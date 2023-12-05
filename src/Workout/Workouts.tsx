7b95edce5fe60681e2f19893fb4910ed70f3fdaf
Can Ivit <canivit@users.noreply.github.com>
2023-12-05 16:27:11 -0500

    implement filter bar for workouts

--- src/Workout/Workouts.tsx
+++ src/Workout/Workouts.tsx
@@ -2,9 +2,27 @@ import { useEffect, useState } from "react";
 import { Level, Workout } from "./types";
 import * as client from "./client";
 import { SimpleLink } from "../SimpleLink";
+import { Link } from "react-router-dom";
+import { TrainerContent } from "../User/TrainerContent";
+import { AthleteContent } from "../User/AthleteContent";
+import { useSelector } from "react-redux";
+import { GlobalState } from "../Store/store";
 
 export function Workouts() {
   const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const [filter, setFilter] = useState<FilterState>({
+    level: "All",
+    showOnlyMyWorkouts: false,
+  });
+
+  const currentUser = useSelector(
+    (state: GlobalState) => state.userReducer.currentUser
+  );
+
+  const workoutPred = createWorkoutPredicate(
+    filter,
+    currentUser ? currentUser._id : false
+  );
 
   async function fetchWorkouts() {
     setWorkouts(await client.findAllWorkouts());
@@ -15,11 +33,90 @@ export function Workouts() {
   }, []);
 
   return (
-    <div className="d-flex flex-row flex-wrap">
-      {workouts.map((workout) => (
-        <WorkoutCard workout={workout} key={workout._id} />
-      ))}
+    <div style={{width: "100%"}}>
+      <TopBar filter={filter} setFilter={setFilter} />
+      <div className="d-flex flex-row flex-wrap">
+        {workouts.filter(workoutPred).map((workout) => (
+          <WorkoutCard workout={workout} key={workout._id} />
+        ))}
+      </div>
     </div>
+  );
+}
+
+function TopBar({
+  filter,
+  setFilter,
+}: {
+  filter: FilterState;
+  setFilter: (filter: FilterState) => void;
+}) {
+  return (
+    <>
+      <div className="d-flex align-items-center">
+        <div className="d-flex align-items-baseline me-5">
+          <label className="form-label me-2" htmlFor="levelInput">
+            Level
+          </label>
+          <select
+            className="form-select"
+            id="levelInput"
+            value={filter.level}
+            onChange={(e) =>
+              setFilter({
+                ...filter,
+                level: e.target.value as Level,
+              })
+            }
+          >
+            <option value="All">All Levels</option>
+            <option value="Beginner">Beginner</option>
+            <option value="Intermediate">Intermediate</option>
+            <option value="Advanced">Advanced</option>
+          </select>
+        </div>
+        <TrainerContent>
+          <div className="form-check me-5">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="showOnlyMyWorkoutsCheckboxForTrainers"
+              checked={filter.showOnlyMyWorkouts}
+              onChange={(e) =>
+                setFilter({ ...filter, showOnlyMyWorkouts: e.target.checked })
+              }
+            />
+            <label
+              className="form-check-label"
+              htmlFor="showOnlyMyWorkoutsCheckboxForTrainers"
+            >
+              Show only workouts I created
+            </label>
+          </div>
+        </TrainerContent>
+        <AthleteContent>
+          <div className="form-check me-5">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="showOnlyMyWorkoutsCheckboxForAthletes"
+            />
+            <label
+              className="form-check-label"
+              htmlFor="showOnlyMyWorkoutsCheckboxForAthletes"
+            >
+              Show only workouts from trainers I follow
+            </label>
+          </div>
+        </AthleteContent>
+        <TrainerContent>
+          <Link to="/CreateWorkout" className="btn btn-primary ms-auto">
+            Create Workout
+          </Link>
+        </TrainerContent>
+      </div>
+      <hr />
+    </>
   );
 }
 
@@ -113,3 +210,28 @@ function formatDate(date: Date): string {
 
   return `${month}/${day}/${year} ${hours}:${minutes}`;
 }
+
+function createWorkoutPredicate(
+  filterState: FilterState,
+  currentTrainerId: string | false
+) {
+  return (workout: Workout) => {
+    if (filterState.level !== "All" && workout.level !== filterState.level) {
+      return false;
+    }
+
+    if (
+      filterState.showOnlyMyWorkouts &&
+      workout.trainer!._id !== currentTrainerId
+    ) {
+      return false;
+    }
+
+    return true;
+  };
+}
+
+type FilterState = {
+  level: Level | "All";
+  showOnlyMyWorkouts: boolean;
+};
